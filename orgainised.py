752e45f603b551ea042122a56b1f899136945248
vyasshivansh25 <118053495+vyasshivansh25@users.noreply.github.com>
2024-02-06 07:31:26 -0800

    Add files via upload

---
+++ orgainised.py
@@ -0,0 +1,19 @@
+import random
+
+
+
+no =random.randint(1,6)
+
+if no==1:
+    print("[-----]")
+print("[    ]")
+print("[0]")
+print("[     ]")
+print("[-----]")
+
+if no==2:
+ print("[-----]")
+print("[  00  ]")
+print("[]")
+print("[     ]")
+print("[-----]")
